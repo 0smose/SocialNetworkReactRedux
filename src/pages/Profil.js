285a95bc4d95e58d140a1f36a1ce590981299bb9
Robin Dercole <robin.dercole@gmail.com>
2020-05-28 16:54:19 +0200

    main features done

--- src/pages/Profil.js
+++ src/pages/Profil.js
@@ -5,15 +5,19 @@ const Profil = () => {
 
     const [token, setToken] = useState('');
     const [username, setUsername] = useState('');
-  
+    const [user, setUser] = useState('');
+    const [userPost, setUserPost] = useState([])
+    const [edit, setEdit] = useState('');
+
+    console.log(user.id)
+    console.log(userPost)
 
     useEffect(() => {
       setToken(Cookies.get("token"));
-      console.log(token);
     }, []);
    
-    useEffect(() => {
-      const myProfil = () => {
+   
+      useEffect(() => {
         fetch('https://api-minireseausocial.mathis-dyk.fr/users/me', {
           method: 'get', 
           headers: {
@@ -23,20 +27,84 @@ const Profil = () => {
         .then((response) => response.json())
         .then((response) => {
           console.log(response)
-          setUsername(response.username)
+          // setUserPost(response)
+         setUsername(response.username)
+         setUser(response)
         })
-      }
-      myProfil()
 
-    })
+
+      }, [token])
+        
+      const handleEdit = (e) => {
+        setEdit(e.target.value)
+      }
+
+      const posts = () => {
+        fetch(`https://api-minireseausocial.mathis-dyk.fr/posts?user.id=${user.id}`, {
+       method: 'get',
+       headers: {
+         'Authorization': `Bearer ${token}`
+       }
+     })
+     .then((response) => response.json())
+     .then((response) => {
+       console.log(response)
+       setUserPost(response)
+     
+     })
+     .catch((error) => console.log(error));
+      }
+
+      const deletePost = (postToDelete) => {
+        fetch(`https://api-minireseausocial.mathis-dyk.fr/posts/${postToDelete}`, {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          },
+        })
+        .then((response) => response.json())
+        .then((response) => {
+          console.log(response)
+        })
+        .catch((error) => console.log(error))
+      }
+      
+
+      const editPost = (postToEdit) => {
+
+        const data = {
+          text: edit
+        }
+        fetch(`https://api-minireseausocial.mathis-dyk.fr/posts/${postToEdit}`, {
+          method: 'put',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          },
+          body: JSON.stringify(data)
+        })
+        .then((response) => response.json())
+        .then((response) => {
+          console.log(response)
+        })
+        .catch((error) => console.log(error))
+
+      }
+     
 
 
 
     return(
 
         <>
-      
         <p> coucou {username}</p>
+        <button onClick={posts}>voir les post</button>
+        {userPost.map((post, id) => (
+          <li key={id}>{post.text} <button onClick={() => deletePost(post.id)}>delete</button> 
+          <input value={edit} onChange={(e) => setEdit(e.target.value)}></input>
+          <button onClick={() => editPost(post.id)}>edit</button></li>
+        ))}
         </>
     )
 
